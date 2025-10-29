6b52dfebe98bc97a9be43f136fadb48dfd02ea40
Jomer Verola <soondevjomer@gmail.com>
2025-10-29 16:26:32 +0800

    Update environment apibaseurl to http.

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://libraryverse-api-production.up.railway.app',  // local Spring Boot API
+  apiBaseUrl: 'http://libraryverse-api-production.up.railway.app',  // local Spring Boot API
   defaultBookCover: 'default-bookcover.png',
   defaultLibraryCover: 'default-librarycover.png',
 };
